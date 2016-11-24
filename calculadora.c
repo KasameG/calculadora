5aca2f1514465e4b1eec6bf2516e9ed5ec808946
Caroline Souza <loh_jg@hotmail.com>
2016-11-24 15:15:13 -0300

    Operação de soma e subtração feitas

--- calculadora.c
+++ calculadora.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
  int main () {
-	 
+	int resultado, a, b;
+	char operacao;
+	scanf("%d %c %d",&a, &operacao, &b);
+	//scanf("%d",&b);
+	switch (operacao) {
+	    case '+':
+	     resultado= a+b;
+	     break;
+	    
+	    case '-':
+	     resultado= a-b;
+	     break;
+	    default: printf("ERRO");
+	    
+	     
+	}
+	 printf("%d",resultado );
 	 return-0;
  }
  
